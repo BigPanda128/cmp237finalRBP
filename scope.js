1bb46fcfe9991886c0ac5ef37ee0478ccdf58c41
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-06 11:48:30 -0400

    commented

--- scope.js
+++ scope.js
@@ -2,13 +2,14 @@
 //CMP237 final exam
 //question 0 scope.js
 
-var x = 30;
+var x = 30;//global variable
 
-function hello() {
+function hello() {//local variable function
 	var x = 69;
 	return x;
 }
 
+//test code
 print('printing out variable x: ' + x);
 print('printing out variable x hiddin in a function: ' + hello());
 
