619c7624c710446a59d8f6e62e3088a80bf54589
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-06 11:37:45 -0400

    question 0 shows loacal and global scope

---
+++ scope.js
@@ -0,0 +1,14 @@
+//Ryan Postma
+//CMP237 final exam
+//question 0 scope.js
+
+var x = 30;
+
+function hello() {
+	var x = 69;
+	return x;
+}
+
+print('printing out variable x: ' + x);
+print('printing out variable x hiddin in a function: ' + hello());
+
