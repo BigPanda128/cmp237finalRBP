d77ece0b4f17968fe56795c0aeb78b1fe5c4ac7a
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-06 11:47:43 -0400

    commented

--- errorScope.js
+++ errorScope.js
@@ -2,15 +2,16 @@
 //CMP237 final exam
 //question 1 error scope
 
-var x = 30;
+var x = 30;//regular global scope
 
-var z = "You're and idiot";
+var z = "You're and idiot";//global scope of z to show your an idiot
 
-function idiot() {
+function idiot() {//this runs if you call z properly
 	var z = 69;
 	return z;
 }
 
+//testing code
 print('print out variable x: ' + x);
 print('printing out variable z from a fucntion: ' + idiot());
 print('printing out variable z in general: ' + z);
